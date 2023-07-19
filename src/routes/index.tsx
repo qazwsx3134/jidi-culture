654d8bec5fa24e25f7924c603072febb73f9ea4a
Peter Tsai <qazwsx3134@gmail.com>
2023-07-19 16:12:44 +0800

    feat: Simple page transition v1

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -36,6 +36,32 @@ export default component$(() => {
     });
 
     gsap.ticker.lagSmoothing(0);
+
+    // book flip animation
+    gsap.set("#bookCover", {
+      perspective: 800,
+    });
+
+    gsap.set([".page-3d"], {
+      transformStyle: "preserve-3d",
+      backfaceVisibility: "hidden",
+    });
+
+    gsap.set([".back-page"], {
+      rotateY: 180,
+    });
+
+    gsap.to(".page-animation", {
+      scrollTrigger: {
+        trigger: "#bookSection",
+        start: "top top",
+        end: "bottom bottom",
+      },
+      translateX: -5,
+      rotationY: 180,
+      transformOrigin: "left center",
+      ease: "none",
+    });
   });
   return (
     <>
@@ -48,9 +74,28 @@ export default component$(() => {
         id="bookSection"
         class="bg-bgGray-500 flex items-center justify-center py-20"
       >
-        <div class="bg-secondary w-[1280px] h-[1080px] rounded-lg shadow-2xl flex p-8 gap-2">
-          <div class="bg-bgWhite-500 flex-1 rounded"></div>
-          <div class="bg-bgWhite-500 flex-1 rounded"></div>
+        <div id="bookContainer" class="w-[1280px] h-[1080px]">
+          <div
+            id="bookCover"
+            class="w-full h-full bg-secondary rounded-lg shadow-2xl flex p-8 gap-2"
+          >
+            <div class="page bg-bgWhite-500 flex-1 rounded">
+              <div class="text-9xl">Hello</div>
+            </div>
+            <div class="flex-1 relative">
+              <div class="page-3d page-animation  w-full h-full bg-slate-500 rounded relative z-10">
+                <div class="page front-page text-6xl absolute w-full h-full bg-red-400 z-[2]">
+                  Front
+                </div>
+                <div class="page back-page absolute text-6xl w-full h-full bg-yellow-400 z-[1]">
+                  Back
+                </div>
+              </div>
+              <div class=" w-full h-full text-6xl bg-slate-500 rounded absolute z-0 top-0">
+                Last Page
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <WhiteBGCurveUp bgColor="bg-bgGray-500" />
@@ -58,9 +103,7 @@ export default component$(() => {
         <Showcase />
       </div>
       <WhiteBGCurveDown bgColor="bg-bgRed-500" />
-      <div id="teamSection" class="bg-bgRed-500 h-[1200px]">
-
-      </div>
+      <div id="teamSection" class="bg-bgRed-500 h-[1200px]"></div>
       <RedBGCurveDown bgColor="bg-bgGray-500" />
     </>
   );
