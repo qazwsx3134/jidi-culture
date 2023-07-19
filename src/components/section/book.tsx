18b346f7bc53a0c5cf5ce3679f7f44494a72cf30
Peter Tsai <qazwsx3134@gmail.com>
2023-07-19 21:34:13 +0800

    feat: optimize book animation

---
+++ src/components/section/book.tsx
@@ -0,0 +1,92 @@
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
+
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+export default component$(() => {
+  useVisibleTask$(() => {
+    // initialize Lenis and register it as a global variable
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    // book flip animation
+    gsap.set("#bookCover", {
+      transformStyle: "preserve-3d",
+      perspective: 1024,
+      webkitBackfaceVisibility: "hidden",
+      backfaceVisibility: "hidden",
+    });
+
+    // 讓頁面翻轉時，頁面的背面不會顯示出來
+    gsap.set([".page-3d"], {
+      transformStyle: "preserve-3d",
+      perspective: 1024,
+      webkitBackfaceVisibility: "hidden",
+      backfaceVisibility: "hidden",
+    });
+
+    // 提前翻轉頁面，讓頁面翻轉時，頁面成為正面
+    gsap.set([".back-page"], {
+      rotateY: 180,
+    });
+
+    gsap.to(".page-animation", {
+      scrollTrigger: {
+        trigger: "#bookSection",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1,
+      },
+      duration: 1,
+      translateX: -5,
+      rotationY: 180,
+      transformOrigin: "left center",
+      ease: "none",
+    });
+  });
+  return (
+    <div id="bookContainer" class="w-[1024px] h-[860px]">
+      <div
+        id="bookCover"
+        class="w-full h-full bg-secondary rounded-lg flex p-8 gap-2"
+      >
+        <div class="flex-1 flex justify-end relative">
+          {/* Empty pages */}
+          <div class="w-full h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[5] top-0" />
+          <div class="w-[99%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[6] top-0" />
+          <div class="w-[98%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[7] top-0" />
+          <div class="w-[97%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[8] top-0" />
+
+          {/* First page */}
+          <div class="w-[96%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[9] top-0 p-3">
+            <div class="w-full h-full bg-[url('/images/book-section/1.webp')] bg-contain bg-no-repeat bg-center rounded" />
+          </div>
+        </div>
+        <div class="flex-1 relative">
+          <div class="page-3d page-animation w-[95%] h-full bg-bgWhite-500 rounded relative z-10">
+            {/* Second page frontside */}
+            <div class="front-page text-6xl absolute w-full h-full bg-bgWhite-500 border border-secondary rounded z-[2] p-3">
+              <div class="w-full h-full bg-[url('/images/book-section/2.webp')] bg-contain bg-no-repeat bg-center rounded" />
+            </div>
+
+            {/* Second page backside */}
+            <div class="back-page absolute text-6xl w-full h-full bg-bgWhite-500 border border-secondary rounded z-[1] p-3">
+              <div class="w-full h-full bg-[url('/images/book-section/3.webp')] bg-contain bg-no-repeat bg-center rounded" />
+            </div>
+          </div>
+
+          {/* Third page */}
+          <div class="w-[96%] h-full text-6xl  border border-secondary rounded absolute z-[9] top-0 p-3">
+            <div class="w-full h-full bg-[url('/images/book-section/4.webp')] bg-contain bg-no-repeat bg-center rounded" />
+          </div>
+
+          {/* Empty pages */}
+          <div class="w-[97%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[8] top-0" />
+          <div class="w-[98%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[7] top-0" />
+          <div class="w-[99%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[6] top-0" />
+          <div class="w-full h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[5] top-0" />
+        </div>
+      </div>
+    </div>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ import GrayBGCurveUp from "~/components/curveDivider/grayBGCurveUp";
 import RedBGCurveDown from "~/components/curveDivider/redBGCurveDown";
 import WhiteBGCurveDown from "~/components/curveDivider/whiteBGCurveDown";
 import WhiteBGCurveUp from "~/components/curveDivider/whiteBGCurveUp";
+import Book from "~/components/section/book";
 import Hero from "~/components/section/hero";
 import Parallax from "~/components/section/parallax";
 import Showcase from "~/components/section/showcase";
@@ -36,67 +37,19 @@ export default component$(() => {
     });
 
     gsap.ticker.lagSmoothing(0);
-
-    // book flip animation
-    gsap.set("#bookCover", {
-      perspective: 800,
-    });
-
-    gsap.set([".page-3d"], {
-      transformStyle: "preserve-3d",
-      backfaceVisibility: "hidden",
-    });
-
-    gsap.set([".back-page"], {
-      rotateY: 180,
-    });
-
-    gsap.to(".page-animation", {
-      scrollTrigger: {
-        trigger: "#bookSection",
-        start: "top top",
-        end: "bottom bottom",
-      },
-      translateX: -5,
-      rotationY: 180,
-      transformOrigin: "left center",
-      ease: "none",
-    });
   });
   return (
     <>
-      <div class=" bg-bgGray-500">
+      <div class=" bg-bgGray-500 w-screen">
         <Parallax />
         <Hero />
       </div>
       <GrayBGCurveUp bgColor="bg-bgWhite-500" />
       <div
         id="bookSection"
-        class="bg-bgGray-500 flex items-center justify-center py-20"
+        class="bg-bgGray-500 flex items-center justify-center py-20 w-full"
       >
-        <div id="bookContainer" class="w-[1280px] h-[1080px]">
-          <div
-            id="bookCover"
-            class="w-full h-full bg-secondary rounded-lg shadow-2xl flex p-8 gap-2"
-          >
-            <div class="page bg-bgWhite-500 flex-1 rounded">
-              <div class="text-9xl">Hello</div>
-            </div>
-            <div class="flex-1 relative">
-              <div class="page-3d page-animation  w-full h-full bg-slate-500 rounded relative z-10">
-                <div class="page front-page text-6xl absolute w-full h-full bg-red-400 z-[2]">
-                  Front
-                </div>
-                <div class="page back-page absolute text-6xl w-full h-full bg-yellow-400 z-[1]">
-                  Back
-                </div>
-              </div>
-              <div class=" w-full h-full text-6xl bg-slate-500 rounded absolute z-0 top-0">
-                Last Page
-              </div>
-            </div>
-          </div>
-        </div>
+        <Book />
       </div>
       <WhiteBGCurveUp bgColor="bg-bgGray-500" />
       <div id="showCaseSection" class="bg-bgWhite-500 h-[1200px] my-20">
