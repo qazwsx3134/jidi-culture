686cd1de5a7e99bddabc292c46afc4a9f1a5d81a
Peter Tsai <qazwsx3134@gmail.com>
2023-07-20 15:23:32 +0800

    feat: fix some book animation

--- src/components/section/book.tsx
+++ src/components/section/book.tsx
@@ -33,7 +33,7 @@ export default component$(() => {
     gsap.to(".page-animation", {
       scrollTrigger: {
         trigger: "#bookSection",
-        start: "top top",
+        start: "-20% top",
         end: "bottom bottom",
         scrub: 1,
       },
@@ -48,43 +48,43 @@ export default component$(() => {
     <div id="bookContainer" class="w-[1024px] h-[860px]">
       <div
         id="bookCover"
-        class="w-full h-full bg-secondary rounded-lg flex p-8 gap-2"
+        class="w-full h-full bg-bgRed-500 rounded-lg flex p-8 gap-2"
       >
         <div class="flex-1 flex justify-end relative">
           {/* Empty pages */}
-          <div class="w-full h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[5] top-0" />
-          <div class="w-[99%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[6] top-0" />
-          <div class="w-[98%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[7] top-0" />
-          <div class="w-[97%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[8] top-0" />
+          <div class="w-full h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[5] top-0" />
+          <div class="w-[99%] h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[6] top-0" />
+          <div class="w-[98%] h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[7] top-0" />
+          <div class="w-[97%] h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[8] top-0" />
 
           {/* First page */}
-          <div class="w-[96%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[9] top-0 p-3">
+          <div class="w-[96%] h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[9] top-0 p-3">
             <div class="w-full h-full bg-[url('/images/book-section/1.webp')] bg-contain bg-no-repeat bg-center rounded" />
           </div>
         </div>
         <div class="flex-1 relative">
           <div class="page-3d page-animation w-[95%] h-full bg-bgWhite-500 rounded relative z-10">
             {/* Second page frontside */}
-            <div class="front-page text-6xl absolute w-full h-full bg-bgWhite-500 border border-secondary rounded z-[2] p-3">
+            <div class="front-page text-6xl absolute w-full h-full bg-bgWhite-500 border border-bgRed-700 rounded z-[2] p-3">
               <div class="w-full h-full bg-[url('/images/book-section/2.webp')] bg-contain bg-no-repeat bg-center rounded" />
             </div>
 
             {/* Second page backside */}
-            <div class="back-page absolute text-6xl w-full h-full bg-bgWhite-500 border border-secondary rounded z-[1] p-3">
+            <div class="back-page absolute text-6xl w-full h-full bg-bgWhite-500 border border-bgRed-700 rounded z-[1] p-3">
               <div class="w-full h-full bg-[url('/images/book-section/3.webp')] bg-contain bg-no-repeat bg-center rounded" />
             </div>
           </div>
 
           {/* Third page */}
-          <div class="w-[96%] h-full text-6xl  border border-secondary rounded absolute z-[9] top-0 p-3">
+          <div class="w-[96%] h-full text-6xl  border border-bgRed-700 rounded absolute z-[9] top-0 p-3">
             <div class="w-full h-full bg-[url('/images/book-section/4.webp')] bg-contain bg-no-repeat bg-center rounded" />
           </div>
 
           {/* Empty pages */}
-          <div class="w-[97%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[8] top-0" />
-          <div class="w-[98%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[7] top-0" />
-          <div class="w-[99%] h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[6] top-0" />
-          <div class="w-full h-full text-6xl bg-bgWhite-500 border border-secondary rounded absolute z-[5] top-0" />
+          <div class="w-[97%] h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[8] top-0" />
+          <div class="w-[98%] h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[7] top-0" />
+          <div class="w-[99%] h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[6] top-0" />
+          <div class="w-full h-full text-6xl bg-bgWhite-500 border border-bgRed-700 rounded absolute z-[5] top-0" />
         </div>
       </div>
     </div>
